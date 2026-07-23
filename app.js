50b3b9f767d6223a99e5a3383ddcdd0f9b77572e
PoistJ <157406020+PoistJ@users.noreply.github.com>
2026-07-23 18:54:51 -0400

    Initialized passport.js in root file and routed temp homepage view.

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ require("dotenv").config();
 const pool = new Pool({
   connectionString: process.env.POOL_STRING,
 });
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "ape", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log("App is live on port 3000");
+});
